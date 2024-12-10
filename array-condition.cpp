ea760863e1747de72f2cb9608d47c2d3a223da7d
YIZ006 <phamc13579@gmail.com>
2024-12-10 15:49:23 +0700

    Add files via upload

---
+++ array-condition.cpp
@@ -0,0 +1,20 @@
+//Tìm kiếm trong mảng  , khai báo một mảng  5 phần tử nguyên ,
+//tìm một giá trị số nguyên nhập từ bàn phím xem
+//có tồn tại trong mảng hay không?
+#include<stdio.h>
+int main(){
+int n,array[5]={1,2,3,4,5};
+printf("Nhap so nguyen n: ");
+scanf("%d",&n);
+for(int i=0;i<5;i++){
+	if(array[i] == n ){ printf("So nguyen co ton tai trong mang");
+return 0;
+}
+}
+printf("So nguyen khong ton tai trong mang");
+}
+arr[0]=1=1
+arr[1]=2=2
+arr[2]=3=3
+3=>4=4
+4=>5=5
